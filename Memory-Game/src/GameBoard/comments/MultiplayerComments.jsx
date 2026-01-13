afd8205081cb5643fe18989700d5a485e81e4bd2
redportz <120698710+redportz@users.noreply.github.com>
2026-01-12 23:28:26 -0500

    changing styling
    dark mode will be worked on

--- Memory-Game/src/GameBoard/comments/MultiplayerComments.jsx
+++ Memory-Game/src/GameBoard/comments/MultiplayerComments.jsx
@@ -4,9 +4,9 @@ function Multiplayer_comments({playerOneScore, moves, playerTwoScore, playerTurn
     return(
 
             <div className="multi-comments">
-            <div className="comments">Player 1 Score: {playerOneScore} </div>
-            <div className="comments">Moves : {moves} </div>
-            <div className="comments">Player 2 Score: {playerTwoScore} </div>
+            <div className="comments player-one">Player 1 Score: {playerOneScore} </div>
+            <div className="comments ">Moves : {moves} </div>
+            <div className="comments player-two">Player 2 Score: {playerTwoScore} </div>
             <div className={`player-indicator ${playerTurn === 1 ? 'player-one' : 'player-two'} comments current-player-display`}>Current Turn: Player {playerTurn}</div>
             </div> 
     )
